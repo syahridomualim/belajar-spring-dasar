ee41d691b3ab95326c6b72bff06850c334527bac
syahridomualim <syahridomualim@gmail.com>
2022-04-25 16:16:18 +0700

    add custom scope module

--- belajar-spring-dasar/src/main/kotlin/mualimsyahrido/spring/core/ScopeConfiguration.kt
+++ belajar-spring-dasar/src/main/kotlin/mualimsyahrido/spring/core/ScopeConfiguration.kt
@@ -2,6 +2,8 @@ package mualimsyahrido.spring.core
 
 import mualimsyahrido.spring.core.data.Bar
 import mualimsyahrido.spring.core.data.Foo
+import mualimsyahrido.spring.core.scope.DoubletonScope
+import org.springframework.beans.factory.config.CustomScopeConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Scope
@@ -11,13 +13,27 @@ class ScopeConfiguration {
 
     @Bean
     @Scope(value = "prototype")
-    fun foo(): Foo {
+    fun foo1(): Foo {
         Log.log.info("create new Foo")
         return Foo()
     }
 
     @Bean
-    @Scope(value = "singleton")
+    @Scope(value = "prototype")
+    fun foo2(): Foo {
+        Log.log.info("create new Foo")
+        return Foo()
+    }
+
+    @Bean
+    fun customScopeConfiguration(): CustomScopeConfigurer {
+        val configurer = CustomScopeConfigurer()
+        configurer.addScope("doubleton", DoubletonScope())
+        return configurer
+    }
+
+    @Bean
+    @Scope("doubleton")
     fun bar(): Bar {
         Log.log.info("create new Bar")
         return Bar()
--- belajar-spring-dasar/src/test/kotlin/mualimsyahrido/spring/core/ScopeTest.kt
+++ belajar-spring-dasar/src/test/kotlin/mualimsyahrido/spring/core/ScopeTest.kt
@@ -29,12 +29,26 @@ class ScopeTest {
 
     @Test
     internal fun testSingletonScope() {
+        val foo1 = applicationContext.getBean("foo2", Foo::class.java)
+        val foo2 = applicationContext.getBean("foo2", Foo::class.java)
+        val foo3 = applicationContext.getBean("foo2", Foo::class.java)
+
+        Assertions.assertSame(foo1, foo2)
+        Assertions.assertSame(foo1, foo3)
+        Assertions.assertSame(foo2, foo3)
+    }
+
+    @Test
+    internal fun testDoubleScope() {
         val bar1 = applicationContext.getBean(Bar::class.java)
         val bar2 = applicationContext.getBean(Bar::class.java)
         val bar3 = applicationContext.getBean(Bar::class.java)
+        val bar4 = applicationContext.getBean(Bar::class.java)
 
-        Assertions.assertSame(bar1, bar2)
         Assertions.assertSame(bar1, bar3)
-        Assertions.assertSame(bar2, bar3)
+        Assertions.assertSame(bar2, bar4)
+
+        Assertions.assertNotSame(bar1, bar2)
+        Assertions.assertNotSame(bar3, bar4)
     }
 }
