ce393c1d013d4eaa31b12e3493d1528c2678c537
syahridomualim <syahridomualim@gmail.com>
2022-04-30 05:43:20 +0700

    add ordere module

---
+++ belajar-spring-dasar/src/main/kotlin/mualimsyahrido/spring/core/processor/PrefixIdGeneratorBeanPostProcessor.kt
@@ -0,0 +1,23 @@
+package mualimsyahrido.spring.core.processor
+
+import mualimsyahrido.spring.core.Log
+import mualimsyahrido.spring.core.aware.IdAware
+import org.springframework.beans.factory.config.BeanPostProcessor
+import org.springframework.core.Ordered
+import java.util.*
+
+class IdGeneratorBeanPostProcessor : BeanPostProcessor, Ordered {
+    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        Log.log.info("Id Generator Processor for Bean $beanName")
+        if (bean is IdAware) {
+            Log.log.info("Set Id Generator Processor for Bean $beanName")
+            val idAware: IdAware = bean
+            idAware.setUserId("PZN- ${UUID.randomUUID().toString()}")
+        }
+        return bean
+    }
+
+    override fun getOrder(): Int {
+        return 2
+    }
+}
